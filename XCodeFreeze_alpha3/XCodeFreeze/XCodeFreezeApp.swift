a34df650351e8ed1c2d41e8c668d7756a3883c89
SuperBox64 <admin@starplayrx.com>
2025-05-14 19:15:44 -0400

    more guard less try

--- XCodeFreeze_alpha3/XCodeFreeze/XCodeFreezeApp.swift
+++ XCodeFreeze_alpha3/XCodeFreeze/XCodeFreezeApp.swift
@@ -2,8 +2,6 @@ import SwiftUI
 
 @main
 struct XCodeFreezeApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
